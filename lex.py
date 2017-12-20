318fcb8d243b9c30b76226c0b63a4df881f3dcf7
axel.bentodas <axel.bentodasilva@he-arc.ch>
2017-12-20 09:18:24 +0100

    Delete the double

--- lex.py
+++ lex.py
@@ -1,34 +1,32 @@
 import ply.lex as lex
 
 reserved_words = (
-    'else',
-    'elseif',
-    'for',
-    'foreach',
-    'if',
-    'print',
-    'sub',
-    'while'
+	'if',
+	'else',
+	'elseif',
+	'foreach',
+	'for',
+	'while',
+	'sub',
+	'print',
 )
 
 tokens = (
     'NUMBER',
     'ADD_OP',
-    'MUL_OP'
+    'MUL_OP',
+	'LETTER'
 )
 
 literals = '()'
-
 
 def t_ADD_OP(t):
     r'\+|-'
     return t
 
-
 def t_MUL_OP(t):
     r'\*|/'
     return t
-
 
 def t_NUMBER(t):
     r'\d+(\.\d+)?'
@@ -39,64 +37,7 @@ def t_NUMBER(t):
         t.value = 0
     return t
 
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-
-t_ignore = ' \t'
-
-
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-
-lex.lex()
-
-if __name__ == "__main__":
-    import sys
-
-    prog = open(sys.argv[1]).read()
-
-    lex.input(prog)
-
-    while 1:
-        tok = lex.token()
-        if not tok: break
-        print("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
-import ply.lex as lex
-
-tokens = (
-    'NUMBER',
-    'ADD_OP',
-    'MUL_OP'
-)
-
-literals = '()'
-
-
-def t_ADD_OP(t):
-    r'\+|-'
-    return t
-
-
-def t_MUL_OP(t):
-    r'\*|/'
-    return t
-
-
-def t_NUMBER(t):
-    r'\d+(\.\d+)?'
-    try:
-        t.value = float(t.value)
-    except ValueError:
-        print("Line %d: Problem while parsing %s!" % (t.lineno, t.value))
-        t.value = 0
-    return t
-
-
+	
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
---
+++ parser.py
@@ -0,0 +1,31 @@
+import ply.yacc as yacc
+
+from lex import tokens
+import AST
+
+def p_statement_print(p):
+    ''' statement : PRINT expression '''
+    p[0] = AST.PrintNode(p[2])
+
+def p_structure(p):
+    ''' structure : WHILE expression '{' programme '}' '''
+    p[0] = AST.WhileNode([p[2],p[4]])
+	
+	
+	
+	
+if __name__ == "__main__":
+    import sys 
+    	
+    prog = open(sys.argv[1]).read()
+    result = yacc.parse(prog)
+    if result:
+        print (result)
+            
+        import os
+        graph = result.makegraphicaltree()
+        name = os.path.splitext(sys.argv[1])[0]+'-ast.pdf'
+        graph.write_pdf(name) 
+        print ("wrote ast to", name)
+    else:
+        print ("Parsing returned no result!")
